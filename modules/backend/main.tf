c2d80fafd875cfe5cff3f947ccac6f7b96040cf9
Pau Duran <79756450+pchs20@users.noreply.github.com>
2025-06-03 19:33:13 +0200

    [S-140] Backend with HTTPS (#9)

--- modules/backend/main.tf
+++ modules/backend/main.tf
@@ -183,33 +183,14 @@ resource "aws_lb_listener" "app_listener" {
   protocol          = "HTTP"
 
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app_tg_2.arn
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
   }
-  #   default_action {
-  #     type = "redirect"
-  #     redirect {
-  #       port        = "443"
-  #       protocol    = "HTTPS"
-  #       status_code = "HTTP_301"
-  #     }
-  #   }
 }
-
-# resource "aws_lb_listener" "app_listener" {
-#   load_balancer_arn = aws_lb.app_alb.arn
-#   port              = 80
-#   protocol          = "HTTP"
-#
-#   default_action {
-#     type = "redirect"
-#     redirect {
-#       port        = "443"
-#       protocol    = "HTTPS"
-#       status_code = "HTTP_301"
-#     }
-#   }
-# }
 
 data "aws_route53_zone" "veyrai_domain" {
   name         = "veyrai.com"
@@ -228,45 +209,44 @@ resource "aws_route53_record" "backend" {
   }
 }
 
-# ToDo (pduran): Configure HTTPS listener with ACM certificate
-# resource "aws_route53_record" "cert_validation" {
-#   for_each = {
-#     for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-#       name   = dvo.resource_record_name
-#       type   = dvo.resource_record_type
-#       record = dvo.resource_record_value
-#     }
-#   }
-#   zone_id = data.aws_route53_zone.veyrai_domain.zone_id
-#   name    = each.value.name
-#   type    = each.value.type
-#   records = [each.value.record]
-#   ttl     = 60
-#   depends_on = [aws_acm_certificate.cert]
-# }
-#
-# resource "aws_acm_certificate_validation" "cert_validation" {
-#   certificate_arn         = aws_acm_certificate.cert.arn
-#   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
-# }
-#
-# resource "aws_acm_certificate" "cert" {
-#   domain_name       = "backend.veyrai.com"
-#   validation_method = "DNS"
-#   tags = {
-#     Environment = var.environment
-#   }
-# }
-#
-# resource "aws_lb_listener" "https_listener" {
-#   load_balancer_arn = aws_lb.app_alb.arn
-#   port              = 443
-#   protocol          = "HTTPS"
-#   ssl_policy        = "ELBSecurityPolicy-2016-08"
-#   certificate_arn   = aws_acm_certificate_validation.cert_validation.certificate_arn
-#
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.app_tg_2.arn
-#   }
-# }
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+  zone_id = data.aws_route53_zone.veyrai_domain.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  ttl     = 60
+  depends_on = [aws_acm_certificate.cert]
+}
+
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
+
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "backend.veyrai.com"
+  validation_method = "DNS"
+  tags = {
+    Environment = var.environment
+  }
+}
+
+resource "aws_lb_listener" "https_listener" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate_validation.cert_validation.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg_2.arn
+  }
+}
