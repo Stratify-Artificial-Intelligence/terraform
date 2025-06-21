82a5f5fcc625658fb295e25751e9574cf4cd2873
pchs20 <pauduranmanzano@gmail.com>
2025-06-21 09:24:15 +0200

    [S-137] Backend with S3 storage

--- modules/common/main.tf
+++ modules/common/main.tf
@@ -17,6 +17,11 @@ resource "aws_iam_role_policy_attachment" "ecs_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+resource "aws_iam_role_policy_attachment" "ecs_s3_access" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
 resource "aws_iam_role_policy" "ecs_secrets_access_all" {
   name = "${var.environment}-ecs-secrets-access-all"
   role = aws_iam_role.ecs_task_execution_role.name
