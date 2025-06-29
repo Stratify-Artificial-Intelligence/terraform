d012f94d80c97f0b4b4e3b8480457b04a5630c95
pchs20 <pauduranmanzano@gmail.com>
2025-06-29 12:04:03 +0200

    [S-147] Dev environment

---
+++ environments/development/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "common" {
+  source      = "../../modules/common"
+  environment = local.environment
+}
+
+module "backend" {
+  source                      = "../../modules/backend"
+  region                      = var.region
+  environment                 = local.environment
+  app_name                    = "backend"
+  domain                      = var.backend_domain
+  ecs_task_execution_role_arn = module.common.ecs_task_execution_role_arn
+  subnet_ids                  = module.common.public_subnet_ids
+  security_group_id           = module.common.app_security_group_id
+  vpc_id                      = module.common.vpc_id
+}
